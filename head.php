8d15a43bbc806ee37d57afbd74e288190f6d4fe9
Unknown <davideamabile81@gmail.com>
2020-10-07 21:56:59 +0200

    ciclo foreach

--- head.php
+++ head.php
@@ -4,7 +4,7 @@
   <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,300&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     <link rel="stylesheet" href="dist/app.css">
     <title>dischi</title>
--- main.php
+++ main.php
@@ -12,9 +12,18 @@ include 'array.php';
       <!-- inizio ciclo -->
       <?php foreach ($database as $key){ ?>
       <div class="box_disk">
+        <div class="ctn_img">
+          <img src="<?php echo $key['poster'];?>" alt="">
+        </div>
+        <!-- stampa del titolo -->
         <h3><?php echo $key['title']; ?></h3>
-
-
+        <!-- /stampa del titolo -->
+        <!-- stampa autore -->
+        <p><?php echo $key['author']; ?></p>
+        <!--/ stampa autore -->
+        <!-- stampo l'anno -->
+        <p><?php echo $key['year']; ?></p>
+        <!-- stampo l'anno -->
       </div>
       <!-- fine box -->
       <?php } ?>
